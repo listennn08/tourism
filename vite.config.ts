3f361cd5d0bac26999a12d8008ac1022e3a63550
listennn08 <listennn08@outlook.com>
2021-11-13 20:36:47 +0800

    chore: update config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'development' ? '/' : '/tourism',
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src')}/`
